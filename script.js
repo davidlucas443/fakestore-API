11e904f6e2242c4e377ae0fdc043a22db04328c4
DAVID LUCAS DE OLIVEIRA SOUSA <david.sousa12@senaisp.edu.br>
2026-03-05 10:34:05 -0300

    feat: create fakestore-API

---
+++ script.js
@@ -0,0 +1,26 @@
+const loadingProduto = document.getElementById('loading');
+const produto = document.getElementById('produto');
+const error = document.getElementById('error');
+
+fetch('https://fakestoreapi.com/products')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    loadingProduto.style.display = 'none';
+
+    produto.innerHTML = data.map(item => `
+        <div class="produto-card">
+            <h2>${item.title}</h2>
+            <p>Preço: $${item.price}</p>
+            <img src="${item.image}" alt="${item.title}" style="max-width: 200px;">
+        </div>
+    `).join('');
+  })
+  .catch(err => {
+    loadingProduto.style.display = 'none';
+    error.innerHTML = `<p>Erro ao carregar os produtos: ${err.message}</p>`;
+  });
