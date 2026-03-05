8ce00f7e5437adb85b2af0c96f84e0443534a337
DAVID LUCAS DE OLIVEIRA SOUSA <david.sousa12@senaisp.edu.br>
2026-03-05 10:57:55 -0300

    feat: add coments

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+// Seleciona os elementos HTML
 const loadingProduto = document.getElementById('loading');
 const produto = document.getElementById('produto');
 const error = document.getElementById('error');
 
+// Faz requisição na API do fake store
 fetch('https://fakestoreapi.com/products')
   .then(response => {
+    // Verifica se a resposta foi bem-sucedida
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
+    // Converte a resposta para JSON
     return response.json();
   })
   .then(data => {
+    // Remove o carregamento da tela
     loadingProduto.style.display = 'none';
 
+    // Cria os cards de produtos com os dados recebidos
     produto.innerHTML = data.map(item => `
         <div class="produto-card">
             <h2>${item.title}</h2>
@@ -21,6 +27,7 @@ fetch('https://fakestoreapi.com/products')
     `).join('');
   })
   .catch(err => {
+    // Caso ocorra erro, mostra a mensagem de erro
     loadingProduto.style.display = 'none';
     error.innerHTML = `<p>Erro ao carregar os produtos: ${err.message}</p>`;
   });
